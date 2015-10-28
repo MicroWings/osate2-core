209ba8fab8b729c896ccfce5a3b0a7cf71e3522b
Joe Seibel <jseibel@sei.cmu.edu>
2015-10-28 13:19:48 -0400

    Allow BusAccess features to be referenced by FlowSpecifications.

--- org.osate.xtext.aadl2/src/org/osate/xtext/aadl2/validation/Aadl2JavaValidator.java
+++ org.osate.xtext.aadl2/src/org/osate/xtext/aadl2/validation/Aadl2JavaValidator.java
@@ -5861,9 +5861,9 @@ public class Aadl2JavaValidator extends AbstractAadl2JavaValidator {
 					Aadl2Package.eINSTANCE.getFlowEnd_Context());
 		} else if (!(flowFeature instanceof DataAccess) && !(flowFeature instanceof AbstractFeature)
 				&& !(flowFeature instanceof FeatureGroup) && !(flowFeature instanceof Parameter)
-				&& !(flowFeature instanceof Port)) {
+				&& !(flowFeature instanceof Port) && !(flowFeature instanceof BusAccess)) {
 			error('\'' + (flowEndContext != null ? flowEndContext.getName() + '.' : "") + flowFeature.getName()
-					+ "' must be a port, parameter, data access, feature group, or abstract feature.", flowEnd,
+					+ "' must be a port, parameter, data access, bus access, feature group, or abstract feature.", flowEnd,
 					Aadl2Package.eINSTANCE.getFlowEnd_Feature());
 		}
 	}
